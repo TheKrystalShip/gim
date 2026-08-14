1e2ba52a924f2d567d8b334fd4f1aa76af04b369
Sjouke IJntema <18188660+DevCas1@users.noreply.github.com>
2026-08-14 22:30:56 +0200

    Stuff

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 [![License: GPL-3.0](https://img.shields.io/badge/License-GPL--3.0-blue.svg)](LICENSE)
 
-> A bash script to mange Godot installs on Linux
+> A bash script to mange and launch Godot editors on Linux
 
-GIM simplifies the process of downloading, starting and removing Godot game engine versions on Linux.
+GIM simplifies the process of managing (downloading/removing) Godot engine versions
+and allows you to launch versions from a single place.
+
+## Project
+
+GIM uses [Argbash](https://github.com/matejak/argbash) for CLI argument support.
+Install it for your distro or build it locally to start using it with GIM.
+
+## Platform support
+
+Currently, only Linux is supported, and only Archlinux is tested.
+
+## Directory Structure
+
+GIM follows [XDG Base Directory](https://specifications.freedesktop.org/basedir-spec/latest/) conventions:
+
+| Location                          | Purpose                                         |
+| --------------------------------- | ----------------------------------------------- |
+| `~/.local/share/gim/editors/`     | Your installed editor versions are stored here  |
--- gim.sh
+++ gim.sh
@@ -1,88 +1,83 @@
 #!/usr/bin/bash
 
-editors_dir="editors/"
-initial_run=false
-editor_file_name="Godot"
+readonly version="0.1.0"
 
-# Check if editor folder exists
-if [ ! -d $editors_dir ]; then
-  # If not, create it
-  initial_run=true
-  echo "Folder $editors_dir does not exist, creating it."
-  `mkdir $editors_dir`
+# ///// Function Definitions \\\\\
+
+find_installed_editors () {
+  # Find files inside $editors_dir starting with "$editor_file_name_start" (Godot)
+  installed_editors=`find "$editors_dir" -type f -iname "$editor_file_name_start"*`
+
+  # Check installed editors array is empty
+  # if [ "${#installed_editors[@]}" -eq 0 ]; then
+  #   # If so, inform and exit
+  #   echo "WARNING: No Godot editors found inside $editors_dir. To use GIM, place editors inside this folder."
+  # fi
+
+}
+
+show_intalled_editors () {
+  # Get installed editors
+  find_installed_editors
+
+  # Print installed editors
+  echo "Installed editors:"
+
+  for ((i = 0 ; i <= ${#installed_editors[@]} ; i++)); do
+    echo "${installed_editors[$i]}"
+    # printf "[$i]: ${installed_editors[$i]}\n"
+  done
+}
+
+# ///// Script Start \\\\\
+
+# Check if XDG_DATA_HOME is set, otherwise use a default
+if [ -z "$XDG_DATA_HOME" ]; then
+ DATA_DIR="$HOME/.local/share"
+else
+  DATA_DIR="$XDG_DATA_HOME"
 fi
 
-# If it's the first run, inform there are no installs present yet
-if [ "$initial_run" -o ]; then
-  echo "Place Godot editors inside $editors_dir to use GIM"
+APP_DATA_DIR="$DATA_DIR/gim"
+
+# Check if application folder exists
+if [ ! -d $APP_DATA_DIR ]; then
+  echo "Application folder does not exist, creating..."
+
+  # Create application-specific directory because it does not exist
+  `mkdir -p $APP_DATA_DIR`
+
+  # Check if creation of directory failed
+  if [ $? -ne 0 ] ; then
+      echo "Could not create $APP_DATA_DIR! Exiting..."
+      exit 1
+  fi
+
+  echo "Created folder $APP_DATA_DIR"
+fi
+
+# Initialize remaining constants/variables
+readonly editors_dir="$APP_DATA_DIR/editors/"
+readonly editor_file_name_start="Godot"
+
+# Check if the folder container editors exists
+if [ ! -d $editors_dir ]; then
+  # If it does not exist, initialize files and folders
+  echo "Path $editors_dir does not exist, creating..."
+  `mkdir -p $editors_dir`
+  if [ $? -ne 0 ] ; then
+      echo Failure
+  fi
+
+  # If directories are just initialized, inform about installation and exit
+  echo "Place Godot editors inside $editors_dir to start using GIM"
   exit 0
 fi
 
-# Find editors starting with "Godot"
-installed_editors=`find . -name '$editor_file_name*'`
 
-# Check installed editors array is empty
-if [ "${#installed_editors[@]}" -eq 0]; then
-  # If so, inform and exit
-  echo "ERROR: No Godot editors found inside $editors_dir"
-  exit 1
-fi
 
-# Print installed editors with index for user to select
-echo -e "Select an editor to launch.\nInstalled editors:"
-
-for ((i = 0 ; i <= ${#installed_editors[@]} ; i++)); do
-  echo "[$i]\t${installed_editors[$i]}"
-done
-
-# Get index input from user
-read index_input
-
-# Parse input to integer number using regex
-input_number='$index_input =~ ^[0-9]+$'
-
-# Exit if input is not an integer number
-if ! [[ $yournumber =~ $re ]] ; then
-   echo "ERROR: Not a number"
-   exit 1
-fi
 
 # Launch selected editor
 # TODO: Implement
 
 exit 0
-
-# ///// For in the future \\\\\
-
-# Check if default version is present
-# If so, start it and exit
-# Otherwise, inform and ask to either download it or select new default
-
-# Bootstrap the environment.
-# shellcheck disable=SC1091
-# source "$(dirname "$(readlink -f "$0")")/lib/bootstrap.sh"
-
-# Load essential modules early
-# module_interactive=$(__find_module interactive.sh)
-
-# installer_script="$(__find_or_fail installer.sh)"
-
-# Check if installer script is present
-# if [[ ! -f "$installer_script" ]]; then
-#   __print_error "installer.sh missing, won't be able to check for updates"
-#   __print_error "Installation might be compromised, please reinstall KGSM"
-#   exit $EC_GENERAL
-# fi
-
-# Basic update functions
-# function check_for_update() {
-#   "$installer_script" --check-update
-# }
-
-# function update_script() {
-#   "$installer_script" --update
-# }
-
-# function get_version() {
-#   "$installer_script" --version
-# }
